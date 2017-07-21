6ab26b85e57ad6f5deccf4f0ace4e3893f6c8541
Santos, Russell <russell.santos@Kronos.com>
2017-07-21 12:51:05 -0400

    Change UI for pages. Finally ready for time with alternating intervals.

--- WorkoutTimer/FifteenthViewController.swift
+++ WorkoutTimer/FifteenthViewController.swift
@@ -23,6 +23,19 @@ class FifteenthViewController: UIViewController,UITextFieldDelegate{
         editCoolDown = true
     }
     
+    @IBOutlet weak var numberOfIntervalsLabel: UILabel!
+    
+    @IBOutlet weak var pickUpLengthLabel: UILabel!
+    
+    @IBOutlet weak var restLengthLabel: UILabel!
+    
+    @IBAction func toEditButton(_ sender: UIButton) {
+        toEdit = true
+    }
+    
+    @IBAction func toRunButton(_ sender: UIButton) {
+        toRun = true
+    }
     
 
     
@@ -43,12 +56,23 @@ class FifteenthViewController: UIViewController,UITextFieldDelegate{
     var isJog = Bool()
     var mainArray = [IncrementDecrement]()
     
+    var toEdit = false
+    var toRun = false
+    
+    var boldText = ""
+    
     var stringWarmUpHours = ""
     var stringWarmUpMinutes = ""
     var stringWarmUpSeconds = ""
     var stringCoolDownHours = ""
     var stringCoolDownMinutes = ""
     var stringCoolDownSeconds = ""
+    var stringPickUpHours = ""
+    var stringPickUpMinutes = ""
+    var stringPickUpSeconds = ""
+    var stringRestHours = ""
+    var stringRestMinutes = ""
+    var stringRestSeconds = ""
 
     var editWarmUp = false
     var editCoolDown = false
@@ -58,11 +82,254 @@ class FifteenthViewController: UIViewController,UITextFieldDelegate{
  
         super.viewDidLoad()
         
-        var j = 0
-        while j < mainArray.count {
-            mainArray[j].toString()
-            j = j + 1
+        var g = 0
+        while g < mainArray.count {
+            mainArray[g].toString()
+            g = g + 1
         }
+        
+        var bText = "Number Of Intervals: "
+        var atrs = [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 17)]
+        var atributedString = NSMutableAttributedString(string:bText, attributes: atrs)
+        
+        var iText = String(numberOfIntervals)
+        var nText = NSMutableAttributedString(string:iText)
+        atributedString.append(nText)
+        numberOfIntervalsLabel.attributedText = atributedString
+        
+        
+        
+        /**********************************************************/
+        
+        
+        
+        
+        if pickUpHours > 0 {
+            if pickUpHours == 1 {
+                stringPickUpHours = String(pickUpHours) + " hour"
+            } else {
+                stringPickUpHours = String(pickUpHours) + " hours"
+            }
+            
+        }
+        if pickUpMinutes > 0 {
+            if pickUpMinutes == 1 {
+                stringPickUpMinutes = String(pickUpMinutes) + " minute"
+            } else {
+                stringPickUpMinutes = String(pickUpMinutes) + " minutes"
+            }
+            
+        }
+        if pickUpSeconds > 0 {
+            if pickUpSeconds == 1 {
+                stringPickUpSeconds = String(pickUpSeconds) + " second"
+            } else {
+                stringPickUpSeconds = String(pickUpSeconds) + " seconds"
+            }
+            
+        }
+        
+        
+        if (isJog) {
+            boldText = "Pick Up Length: \n"
+        } else {
+            boldText = "Interval Length: \n"
+        }
+
+        var attrs = [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 17)]
+        var attributedString = NSMutableAttributedString(string:boldText, attributes: attrs)
+        
+        
+        
+        /*if the user is doing a warmup that involves hours, minutes and seconds*/
+        if (stringPickUpHours != "") && (stringPickUpMinutes != "") && (stringPickUpSeconds != "") {
+            
+            var pickUpText = stringPickUpHours + ", " + stringPickUpMinutes + ", and " + stringPickUpSeconds
+            var normalText = NSMutableAttributedString(string:pickUpText)
+            attributedString.append(normalText)
+            pickUpLengthLabel.attributedText = attributedString
+            
+        }
+            
+            /* these are the conditions if one of the hours, minutes,
+             or seconds are empty*/
+        else if (stringPickUpHours == "") && (stringPickUpMinutes != "") && (stringPickUpSeconds != ""){
+            
+            var pickUpText = stringPickUpMinutes + ", and " + stringPickUpSeconds
+            var normalText = NSMutableAttributedString(string:pickUpText)
+            attributedString.append(normalText)
+            pickUpLengthLabel.attributedText = attributedString
+            
+            
+        } else if (stringPickUpHours != "") && (stringPickUpMinutes == "") && (stringPickUpSeconds != ""){
+            
+            var pickUpText = stringPickUpHours + ", and " + stringPickUpSeconds
+            var normalText = NSMutableAttributedString(string:pickUpText)
+            attributedString.append(normalText)
+            pickUpLengthLabel.attributedText = attributedString
+            
+        } else if (stringPickUpHours != "") && (stringPickUpMinutes != "") && (stringPickUpSeconds == ""){
+            
+            var pickUpText = stringPickUpHours + ", and " + stringPickUpMinutes
+            var normalText = NSMutableAttributedString(string:pickUpText)
+            attributedString.append(normalText)
+            pickUpLengthLabel.attributedText = attributedString
+        }
+            
+            
+            /*these are the conditions if two of the conditions, hours, minutes,
+             or seconds are empty */
+        else if (stringPickUpHours == "") && (stringPickUpMinutes == "") && (stringPickUpSeconds != ""){
+            
+            var pickUpText = stringPickUpSeconds
+            var normalText = NSMutableAttributedString(string:pickUpText)
+            attributedString.append(normalText)
+            pickUpLengthLabel.attributedText = attributedString
+            
+        } else if (stringPickUpHours != "") && (stringPickUpMinutes == "") && (stringPickUpSeconds == ""){
+            
+            var pickUpText = stringPickUpHours
+            var normalText = NSMutableAttributedString(string:pickUpText)
+            attributedString.append(normalText)
+            pickUpLengthLabel.attributedText = attributedString
+            
+        } else if (stringPickUpHours == "") && (stringPickUpMinutes != "") && (stringPickUpSeconds == ""){
+            
+            var pickUpText = stringPickUpMinutes
+            var normalText = NSMutableAttributedString(string:pickUpText)
+            attributedString.append(normalText)
+            pickUpLengthLabel.attributedText = attributedString
+            
+        }
+            
+            /*if the user is not doingn a warmup */
+        else {
+            
+            var pickUpText = "None"
+            var normalText = NSMutableAttributedString(string:pickUpText)
+            attributedString.append(normalText)
+            pickUpLengthLabel.attributedText = attributedString
+        }
+
+        /********************************************************/
+        
+        
+        
+        
+        /**********************************************************/
+        
+        
+        
+        
+        if jogHours > 0 {
+            if jogHours == 1 {
+                stringRestHours = String(jogHours) + " hour"
+            } else {
+                stringRestHours = String(jogHours) + " hours"
+            }
+            
+        }
+        if jogMinutes > 0 {
+            if jogMinutes == 1 {
+                stringRestMinutes = String(jogMinutes) + " minute"
+            } else {
+                stringRestMinutes = String(jogMinutes) + " minutes"
+            }
+            
+        }
+        if jogSeconds > 0 {
+            if jogSeconds == 1 {
+                stringRestSeconds = String(jogSeconds) + " second"
+            } else {
+                stringRestSeconds = String(jogSeconds) + " seconds"
+            }
+            
+        }
+        if (isJog) {
+            boldText = "Jogging Length: \n"
+        } else {
+            boldText = "Rest Length: \n"
+        }
+        
+        attrs = [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 17)]
+        attributedString = NSMutableAttributedString(string:boldText, attributes: attrs)
+        
+        
+        
+        /*if the user is doing a warmup that involves hours, minutes and seconds*/
+        if (stringRestHours != "") && (stringRestMinutes != "") && (stringRestSeconds != "") {
+            
+            var restText = stringRestHours + ", " + stringRestMinutes + ", and " + stringRestSeconds
+            var normalText = NSMutableAttributedString(string:restText)
+            attributedString.append(normalText)
+            restLengthLabel.attributedText = attributedString
+            
+        }
+            
+            /* these are the conditions if one of the hours, minutes,
+             or seconds are empty*/
+        else if (stringRestHours == "") && (stringRestMinutes != "") && (stringRestSeconds != ""){
+            
+            var restText = stringRestMinutes + ", and " + stringRestSeconds
+            var normalText = NSMutableAttributedString(string:restText)
+            attributedString.append(normalText)
+            restLengthLabel.attributedText = attributedString
+            
+            
+        } else if (stringRestHours != "") && (stringRestMinutes == "") && (stringRestSeconds != ""){
+            
+            var restText = stringRestHours + ", and " + stringRestSeconds
+            var normalText = NSMutableAttributedString(string:restText)
+            attributedString.append(normalText)
+            restLengthLabel.attributedText = attributedString
+            
+        } else if (stringRestHours != "") && (stringRestMinutes != "") && (stringRestSeconds == ""){
+            
+            var restText = stringRestHours + ", and " + stringRestMinutes
+            var normalText = NSMutableAttributedString(string:restText)
+            attributedString.append(normalText)
+            restLengthLabel.attributedText = attributedString
+        }
+            
+            
+            /*these are the conditions if two of the conditions, hours, minutes,
+             or seconds are empty */
+        else if (stringRestHours == "") && (stringRestMinutes == "") && (stringRestSeconds != ""){
+            
+            var restText = stringRestSeconds
+            var normalText = NSMutableAttributedString(string:restText)
+            attributedString.append(normalText)
+            restLengthLabel.attributedText = attributedString
+            
+        } else if (stringRestHours != "") && (stringRestMinutes == "") && (stringRestSeconds == ""){
+            
+            var restText = stringRestHours
+            var normalText = NSMutableAttributedString(string:restText)
+            attributedString.append(normalText)
+            restLengthLabel.attributedText = attributedString
+            
+        } else if (stringRestHours == "") && (stringRestMinutes != "") && (stringRestSeconds == ""){
+            
+            var restText = stringRestMinutes
+            var normalText = NSMutableAttributedString(string:restText)
+            attributedString.append(normalText)
+            restLengthLabel.attributedText = attributedString
+            
+        }
+            
+            /*if the user is not doingn a warmup */
+        else {
+            
+            var restText = "None"
+            var normalText = NSMutableAttributedString(string:restText)
+            attributedString.append(normalText)
+            restLengthLabel.attributedText = attributedString
+        }
+        
+        /********************************************************/
+
+        
+       
         
         if warmUpHours > 0 {
             if warmUpHours == 1 {
@@ -89,9 +356,9 @@ class FifteenthViewController: UIViewController,UITextFieldDelegate{
             
         }
         
-        var boldText = "Warmup Time: \n"
-        var attrs = [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 17)]
-        var attributedString = NSMutableAttributedString(string:boldText, attributes: attrs)
+        boldText = "Warmup Time: \n"
+        attrs = [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 17)]
+        attributedString = NSMutableAttributedString(string:boldText, attributes: attrs)
         
         NSLog("Warmup Hours: " + stringWarmUpHours);
         NSLog("Warmup Minutes: " + stringWarmUpMinutes);
@@ -333,8 +600,8 @@ class FifteenthViewController: UIViewController,UITextFieldDelegate{
             destination.fromFifteen = true
             
             
-        } else {
-            let destination : EleventhViewController = segue.destination as! EleventhViewController
+        } else if toEdit {
+            let destination : SeventhViewController = segue.destination as! SeventhViewController
             NSLog("in segue")
             destination.usersName = String(usersName)
             destination.warmUpSeconds = warmUpSeconds
@@ -343,12 +610,9 @@ class FifteenthViewController: UIViewController,UITextFieldDelegate{
             destination.coolDownSeconds = coolDownSeconds
             destination.coolDownMinutes = coolDownMinutes
             destination.coolDownHours = coolDownHours
+            destination.isJog = isJog
         }
-        
     }
-
-
-    
 }
 
 
